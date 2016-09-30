7acf450a91363afb6c8163626ae0723d90def5fd
Julia Kozhukhovskaya <jkozhukhovskaya@gmail.com>
2016-09-30 16:28:49 -0700

    Add linear layout

--- app/src/main/java/com/example/julia/popularmovies/MovieDetailFragment.java
+++ app/src/main/java/com/example/julia/popularmovies/MovieDetailFragment.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2016.  Julia Kozhukhovskaya
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.example.julia.popularmovies;
 
 import android.content.Intent;
@@ -26,8 +42,8 @@ public class MovieDetailFragment extends Fragment {
             Movie movie = intent.getParcelableExtra(Intent.EXTRA_TEXT);
 
             // Set Movie Title
-            ((TextView) rootView.findViewById(R.id.detail_title))
-                    .setText(movie.title);
+            //((TextView) rootView.findViewById(R.id.detail_title))
+            //        .setText(movie.title);
 
             // Set Movie Release date
             ((TextView) rootView.findViewById(R.id.detail_release_date))
--- app/src/main/java/com/example/julia/popularmovies/data/MovieContract.java
+++ app/src/main/java/com/example/julia/popularmovies/data/MovieContract.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2016.  Julia Kozhukhovskaya
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.example.julia.popularmovies.data;
 
 import android.content.ContentResolver;
@@ -7,32 +23,17 @@ import android.provider.BaseColumns;
 
 public class MovieContract {
 
-    // The "Content authority" is a name for the entire content provider, similar to the
-    // relationship between a domain name and its website.  A convenient string to use for the
-    // content authority is the package name for the app, which is guaranteed to be unique on the
-    // device.
     public static final String CONTENT_AUTHORITY = "com.example.julia.popularmovies";
-
-    // Use CONTENT_AUTHORITY to create the base of all URI's which apps will use to contact
-    // the content provider.
     private static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
-
-    // Possible paths (appended to base content URI for possible URI's)
-    // For instance, content://com.example.julia.popularmovies/movie/ is a valid path for
-    // looking at movie data. content://com.example.julia.popularmovies/givemeroot/ will fail,
-    // as the ContentProvider hasn't been given any information on what to do with "givemeroot".
-    // At least, let's hope not.  Don't be that dev, reader.  Don't be that dev.
     static final String PATH_MOVIE = "movie";
 
     public static final class MovieEntry implements BaseColumns {
+
         public static final Uri CONTENT_URI =
                 BASE_CONTENT_URI.buildUpon().appendPath(PATH_MOVIE).build();
 
         public static final String CONTENT_TYPE =
                 ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_MOVIE;
-        public static final String CONTENT_ITEM_TYPE =
-                ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_MOVIE;
-
 
         public static final String TABLE_NAME = "movie";
         public static final String COLUMN_MOVIE_ID = "movie_id";
