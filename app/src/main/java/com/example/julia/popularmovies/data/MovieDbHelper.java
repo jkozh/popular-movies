bd47626ad451a30f16f9d9e548a2de1e3c78c1b4
Julia Kozhukhovskaya <julia.kozh@yandex.ru>
2016-09-30 22:17:27 -0700

    Upgrade database's onUpgrade function

--- app/src/main/java/com/example/julia/popularmovies/data/MovieDbHelper.java
+++ app/src/main/java/com/example/julia/popularmovies/data/MovieDbHelper.java
@@ -19,12 +19,14 @@ package com.example.julia.popularmovies.data;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
 
 import com.example.julia.popularmovies.data.MovieContract.MovieEntry;
 
 public class MovieDbHelper extends SQLiteOpenHelper {
-    private static final int DATABASE_VERSION = 1;
+    public static final String LOG_TAG = MovieDbHelper.class.getSimpleName();
 
+    private static final int DATABASE_VERSION = 1;
     public static final String DATABASE_NAME = "movie.db";
 
     public MovieDbHelper(Context context) {
@@ -47,7 +49,15 @@ public class MovieDbHelper extends SQLiteOpenHelper {
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+        Log.w(LOG_TAG, "Upgrading database from version " + oldVersion + " to " +
+                newVersion + ". OLD DATA WILL BE DESTROYED");
+
+        // drop the table
         db.execSQL("DROP TABLE IF EXISTS " + MovieEntry.TABLE_NAME);
+        db.execSQL("DELETE FROM SQLITE_SEQUENCE WHERE NAME = '" + MovieEntry.TABLE_NAME + "'");
+
+        // re-create database
         onCreate(db);
     }
 }
